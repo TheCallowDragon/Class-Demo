8f3c14c251cb496579ca48b3edf65ea9c927cf79
Jaeden <JaedenEllis@outlook.com>
2024-04-24 14:21:32 -0700

    Galactic Map Iframe

--- script.js
+++ script.js
@@ -16,5 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-});
+   
+    
+        var toggleButton = document.getElementById('toggle-button');
+        var iframe = document.getElementById('iframe');
+    
+        // Add event listener to the button
+        toggleButton.addEventListener('click', function() {
+            // Toggle the visibility of the iframe
+            if (iframe.style.display === 'none') {
+                iframe.style.display = 'block';
+            } else {
+                iframe.style.display = 'none';
+            }
+        });
+      
+        var iframe = document.getElementById('iframe');
+        iframe.contentWindow.scrollTo(0, iframe.contentDocument.body.scrollHeight / 2);
 
+});
