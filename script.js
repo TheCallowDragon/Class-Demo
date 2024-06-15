0b498dc045291adc7b7c6a59c26b11ac16a4b0d1
unknown <JaedenEllis@outlook.com>
2024-06-14 22:21:47 -0700

    Final push: Fixed code identation

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-   
-    
-        var toggleButton = document.getElementById('toggle-button');
-        var iframe = document.getElementById('iframemap');
-    
-        // Add event listener to the button
-        toggleButton.addEventListener('click', function() {
-            // Toggle the visibility of the iframe
-            if (iframe.style.display === 'none') {
-                iframe.style.display = 'block';
-            } else {
-                iframe.style.display = 'none';
-            }
-        });
-      
+
+
+    var toggleButton = document.getElementById('toggle-button');
+    var iframe = document.getElementById('iframemap');
+
+    // Add event listener to the button
+    toggleButton.addEventListener('click', function () {
+        // Toggle the visibility of the iframe
+        if (iframe.style.display === 'none') {
+            iframe.style.display = 'block';
+        } else {
+            iframe.style.display = 'none';
+        }
+    });
+
 
 });
 
